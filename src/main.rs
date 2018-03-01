131471632494829f1dca57cd4c0509e46f98852e
Tim Caswell <tim@creationix.com>
2018-03-01 14:42:20 -0600

    Add key exchange sample

--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,21 @@ use crypto::ed25519;
 use crypto::blake2b::Blake2b;
 
 fn main() {
-    let seed = rand::random::<[u8; 32]>();
-    let (private, public) = ed25519::keypair(&seed);
-    println!("{:x}", Hex(&seed));
-    println!("{:x}", Hex(&public));
-    println!("{:x}", Hex(&private));
+    // Generate a pair of keypairs.
+    let bob = ed25519::keypair(&rand::random::<[u8; 32]>());
+    println!("{:x}", Hex(&bob.0));
+    println!("{:x}", Hex(&bob.1));
+    let alice = ed25519::keypair(&rand::random::<[u8; 32]>());
+    println!("{:x}", Hex(&alice.0));
+    println!("{:x}", Hex(&alice.1));
+
+    // Calculated shared keys using public keys
+    let shared1 = ed25519::exchange(&bob.1, &alice.0);
+    let shared2 = ed25519::exchange(&alice.1, &bob.0);
+    println!("{:x}", Hex(&shared1));
+    println!("{:x}", Hex(&shared2));
+
+    // Take the blake2b hash of a value
     let mut hash = [0u8; 32];
     Blake2b::blake2b(&mut hash, b"Hello", b"");
     println!("{:x}", Hex(&hash));
