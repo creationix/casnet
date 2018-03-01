1d35ec8cb9358ec7fd6a84b1fd286000fbc8de5c
Tim Caswell <tim@creationix.com>
2018-03-01 14:35:32 -0600

    Add blake2b example

--- src/hex.rs
+++ src/hex.rs
@@ -1,10 +1,10 @@
-use std::fmt;
+use std::fmt::{Formatter, LowerHex, Result};
 
 // Simple wrapper around slices to allow printing as hex strings.
 pub struct Hex<'a>(pub &'a [u8]);
 
-impl<'a> fmt::LowerHex for Hex<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+impl<'a> LowerHex for Hex<'a> {
+    fn fmt(&self, f: &mut Formatter) -> Result {
         for byte in self.0 {
             write!(f, "{:02x}", byte)?;
         }
@@ -19,8 +19,9 @@ mod tests {
 
     #[test]
     fn it_works() {
+        let original = b"\x01\x23\x45\x67\x89\xab\xcd\xef";
         let mut s = String::new();
-        write!(&mut s, "{:x}", Hex(b"\x01\x23\x45\x67\x89\xab\xcd\xef")).expect("Problem writing");
+        write!(&mut s, "{:x}", Hex(original)).expect("Problem writing");
         assert_eq!(s, "0123456789abcdef".to_string());
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ extern crate rand;
 
 mod hex;
 
-use crypto::ed25519;
 use hex::Hex;
+use crypto::ed25519;
+use crypto::blake2b::Blake2b;
 
 fn main() {
     let seed = rand::random::<[u8; 32]>();
@@ -12,4 +13,7 @@ fn main() {
     println!("{:x}", Hex(&seed));
     println!("{:x}", Hex(&public));
     println!("{:x}", Hex(&private));
+    let mut hash = [0u8; 32];
+    Blake2b::blake2b(&mut hash, b"Hello", b"");
+    println!("{:x}", Hex(&hash));
 }
