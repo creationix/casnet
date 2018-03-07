4305c38e5d3d0828f4d4c2b9a73278d8a37a7a4d
Tim Caswell <tim@creationix.com>
2018-03-07 09:13:36 -0600

    Save changes

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 extern crate crypto;
 extern crate rand;
+use std::time::{SystemTime, UNIX_EPOCH};
+
+// #[macro_use]
+// extern crate bincode;
+// extern crate serde_derive;
+// use bincode::{deserialize, serialize, Infinite};
 
 mod hex;
 mod peer;
 // mod hash;
+use std::collections::BTreeMap;
 
 use peer::{keypair, PublicKey, SecretKey};
 
@@ -11,51 +18,73 @@ use crypto::chacha20poly1305::ChaCha20Poly1305;
 use hex::Hex;
 use crypto::aead::{AeadDecryptor, AeadEncryptor};
 
-// output vec contains:
-//   from: 32 bytes public key
-//   to: 32 bytes public key
-//   nonce: 8 random bytes
-//   tag: 16 bytes authenticationt tag
-//   encrypted payload: n bytes
-fn make_parcel(sender: (&PublicKey, &SecretKey), receiver: &PublicKey, message: &[u8]) -> Vec<u8> {
-    let (sender, sender_secret) = sender;
-    let mut parcel = Vec::new();
-    parcel.extend_from_slice(&sender);
-    parcel.extend_from_slice(&receiver);
-    let nonce = rand::random::<[u8; 8]>();
-    let shared = sender_secret.exchange(&receiver);
-    let mut channel = ChaCha20Poly1305::new(&shared, &nonce, &parcel);
-    parcel.extend_from_slice(&nonce);
-    let mut output = vec![0; message.len()];
-    let mut tag = [0u8; 16];
-    channel.encrypt(message, &mut output, &mut tag);
-    parcel.extend_from_slice(&tag);
-    parcel.extend_from_slice(&output);
-    parcel
+struct Parcel {
+    sender: PublicKey,
+    receiver: PublicKey,
+    timestamp: u32,
+    nonce: [u8; 8],
+    tag: [u8; 16],
+    payload: Vec<u8>,
 }
 
-fn check_parcel(receiver: (&PublicKey, &SecretKey), parcel: &[u8]) -> Option<(PublicKey, Vec<u8>)> {
-    if parcel.len() < (32 + 32 + 8 + 16) {
-        return None;
-    }
-    let (receiver, receiver_secret) = receiver;
-    let sender = PublicKey::new(&parcel[0..32]);
-    let address: &[u8] = &parcel[32..64];
-    let header: &[u8] = &parcel[0..64];
-    let nonce: &[u8] = &parcel[64..72];
-    let tag: &[u8] = &parcel[72..88];
-    let payload: &[u8] = &parcel[88..];
-    if &receiver as &[u8] != address {
-        return None;
+impl Parcel {
+    fn new(sender: (&PublicKey, &SecretKey), receiver: &PublicKey, message: &[u8]) -> Self {
+        let (sender, sender_secret) = sender;
+        let nonce = rand::random::<[u8; 8]>();
+        let shared = sender_secret.exchange(&receiver);
+        let mut channel = ChaCha20Poly1305::new(&shared, &nonce, &parcel);
+        let timestamp = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
+        let mut payload = vec![0; message.len()];
+        let mut tag = [0u8; 16];
+        channel.encrypt(message, &mut payload, &mut tag);
+        Parcel {
+            sender,
+            receiver,
+            nonce,
+            timestamp,
+            tag,
+            payload,
+        }
     }
 
-    let shared = receiver_secret.exchange(&sender);
-    let mut channel = ChaCha20Poly1305::new(&shared, nonce, header);
-    let mut output = vec![0; payload.len()];
-    if channel.decrypt(payload, &mut output, tag) {
-        Some((sender, output))
-    } else {
-        None
+    fn check(
+        &self,
+        recent: &mut BTreeMap<[u8; 8], u32>,
+        receiver: (&PublicKey, &SecretKey),
+    ) -> (PublicKey, Vec<u8>) {
+        let now = SystemTime::now()
+            .duration_since(UNIX_EPOCH)
+            .expect("Time went backwards")
+            .as_secs();
+        if now > now + 60 || now < now - 60 {
+            // If the timestamp is stale, ignore the message.
+            return None;
+        }
+        if let Some(time) = recent.get(nonce) {
+            if *time == timestamp {
+                // If the message is a replay, ignore it.
+                return None;
+            }
+        }
+        let mut copy = [0u8; 8];
+        copy.copy_from_slice(nonce);
+        recent.insert(copy, timestamp);
+        if &receiver as &[u8] != address {
+            // If it's not addressed to us, ignore it.
+            return None;
+        }
+        let shared = receiver_secret.exchange(&sender);
+        let mut channel = ChaCha20Poly1305::new(&shared, nonce, header);
+        let mut output = vec![0; payload.len()];
+        if channel.decrypt(payload, &mut output, tag) {
+            Some((sender, output))
+        } else {
+            // Ignore malformed messages.
+            None
+        }
     }
 }
 
@@ -63,10 +92,14 @@ fn main() {
     let (alice, alice_secret) = keypair();
     let (bob, bob_secret) = keypair();
 
-    let parcel = make_parcel((&alice, &alice_secret), &bob, b"Hello World");
+    let parcel = Parcel::new((&alice, &alice_secret), &bob, b"Hello World");
 
-    println!("{:x}", Hex(&parcel));
+    println!("{:?}", parcel);
 
-    let res = check_parcel((&bob, &bob_secret), &parcel);
+    let mut recent = BTreeMap::new();
+
+    let res = parcel.check(&mut recent, (&bob, &bob_secret));
     println!("{:?}", res);
+    let res2 = parcel.check(&mut recent, (&bob, &bob_secret));
+    println!("{:?}", res2);
 }
